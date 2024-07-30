58dc3c5eb8c32f0cce34705a1b947536d4725699
zsalstj <stjepan.salopek.ext@ericsson.com>
2024-07-30 17:32:07 +0200

    String interpolation & integer builtins

--- learning-ruby/main.rb
+++ learning-ruby/main.rb
@@ -8,7 +8,20 @@
 
 # Print a string
 puts "Hello world"
+# String interpolation only works for "
+world_type = 'Mars'
+puts "Hello #{world_type}"
+puts 'Hello #{world_type}'
 # Put a newline to separate other code
+puts
+
+
+
+x = 1
+y = 2
+puts "x + y = " + x.to_s + y.to_s
+#puts "x + y = " + x + y            #! no implicit conversion of Integer into String (TypeError)
+puts "x + y = " + (x + y).to_s
 puts
 
 
