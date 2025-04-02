94fbb3fc96f2305c441221480e90d2973d80b854
shtef21 <stjepan.salopek@hotmail.com>
2025-04-02 14:29:36 +0200

    init install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+
+# Install build-essential
+
+# The `build-essential` package is a meta-package in Debian and Ubuntu that includes essential tools for software development
+# Compiler: gcc, g++
+# Make: make
+
+```sh
+sudo apt-get update
+sudo apt-get install build-essential
+```
+
+
+# Install and init rbenv
+
+```sh
+sudo apt-get install rbenv
+rbenv init
+# Reload console
+```
+
+
+# Run ruby!
+
+```sh
+ruby console-stuff/main.rb
+```
+
+
+# Install rails
+
+```sh
+```
+
