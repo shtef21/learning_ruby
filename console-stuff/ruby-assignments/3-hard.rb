a833807f40910106bdf4e990f67f103084dd9610
shtef21 <stjepan.salopek@hotmail.com>
2025-04-16 19:50:03 +0200

    Start working on Tic-Tac-Toe

--- console-stuff/ruby-assignments/3-hard.rb
+++ console-stuff/ruby-assignments/3-hard.rb
@@ -39,3 +39,76 @@ Board:
 ...
 =end
 
+class TicTacToe
+  
+   def initialize
+      @grid = [
+         " ", " ", " ",
+         " ", " ", " ",
+         " ", " ", " "
+      ]
+      @next_player = "X"
+   end
+
+   def intro_grid
+      puts "Board grid:"
+      puts " 1 | 2 | 3"
+      puts "-----------"
+      puts " 4 | 5 | 6"
+      puts "-----------"
+      puts " 7 | 8 | 9"
+      puts
+   end
+
+   def draw
+      puts " #{@grid[0..2].join(' | ')}"
+      puts "-----------"
+      puts " #{@grid[3..5].join(' | ')} "
+      puts "-----------"
+      puts " #{@grid[6..8].join(' | ')} "
+      puts
+   end
+
+   def take_turn
+      next_pl = @next_player == "X" ? 1 : 2
+      print "Player #{next_pl} turn (1-9):"
+      turn = gets.chomp.to_i
+
+      while @grid[turn - 1] != " "
+        print "  Cannot play a non-empty field #{turn}. Try again: "
+        turn = gets.chomp.to_i
+      end
+      
+      @grid[turn - 1] = @next_player
+      @next_player = @next_player == "O" ? "X" : "O"
+   end
+
+   def can_continue
+      @grid.find { |ch| ch == " " }
+   end
+
+   # Check if winner found:
+   #   return "X" -> player 1 wins
+   #   return "O" -> player 2 wins
+   #   return " " -> no winner
+   #   return nil -> game not over yet
+   def check_win
+      # TODO: implement check win
+   end
+
+   def start_game_loop
+     while self.can_continue
+         self.take_turn
+         self.draw
+     end
+     puts "Game over!"
+   end
+end
+
+puts "Tic-Tac-Toe game starting..."
+game = TicTacToe.new
+game.intro_grid
+game.start_game_loop
+
+
+
