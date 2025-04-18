9c1afee404430a42adc026c2309052956f0b923e
shtef21 <stjepan.salopek@hotmail.com>
2025-04-18 16:40:08 +0200

    Solve task 8

--- console-stuff/ruby-assignments/8-medium-anagrams.rb
+++ console-stuff/ruby-assignments/8-medium-anagrams.rb
@@ -4,7 +4,31 @@ Problem:
 Group words that are anagrams of each other.
 
 Input:
-["listen", "enlist", "hello", "silent", "olelh"]
+listen, enlist, hello, silent, olelh
 Output:
 [["listen", "enlist", "silent"], ["hello", "olelh"]]
 =end
+
+def word_value(word)
+  word.chars.sort.join
+end
+
+def group_value(group)
+  # Each word has same value, so take first
+  word_value(group.first)
+end
+
+all_groups = []
+print "Input (WORD_1, WORD_2, ...): "
+input_arr = gets.split(",").map {|w| w.strip.downcase}
+
+input_arr.each do |w|
+  group_idx = all_groups.find_index{|gr| group_value(gr) == word_value(w)}
+  if group_idx.nil?
+    all_groups << [w]  # Add a new group
+  else
+    all_groups[group_idx] << w  # Append to a group
+  end
+end
+
+puts "Output: #{all_groups}"
